83a537a8d8ab940fd49507d6d15fb5322d65b969
A-tak <hoge+git@a-tak.com>
2022-01-27 23:12:22 +0900

    first commit

---
+++ Comp/colorful-clip.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import DaVinciResolveScript as dvr_script
+resolve = dvr_script.scriptapp("Resolve")
+projectManager = resolve.GetProjectManager()
+project = projectManager.GetCurrentProject()
+mediapool = project.GetMediaPool()
+folder = mediapool.GetCurrentFolder()
+color = ["Orange","Blue","Apricot","Navy","Yellow","Teal","Brown","Lime","Purple","Beige","Olive","Violet","Green","Chocolate","Pink","Tan"]
+i = 0
+for clip in folder.GetClipList():
+    print(i)
+    clip.SetClipColor(color[i])
+    i+=1
+    if len(color) < i+1:
+        i=0
