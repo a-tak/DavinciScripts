68968692146167b0aa6975d55cc825e29c88f632
A-tak <hoge+git@a-tak.com>
2022-02-13 22:08:39 +0900

    念のために音声と映像のFPSを分離

--- Comp/offset-tc.py
+++ Comp/offset-tc.py
@@ -4,11 +4,12 @@ import DaVinciResolveScript as dvr_script
 from timecode import Timecode
 
 # ここにタイムコードとフレームレートを手打ち(笑)
-videoTc = "12:44:29:44"
-audioTc = "12:44:39:36"
-fps=59.94
+videoTc = "12:44:39:36"
+audioTc = "12:46:36:38"
+videoFps=59.94
+audioFps=60
 
-diff = Timecode(fps, audioTc) - Timecode(fps, videoTc)
+diff = Timecode(videoFps, audioTc) - Timecode(audioFps, videoTc)
 print("DiffTC",diff)
 
 resolve = dvr_script.scriptapp("Resolve")
@@ -19,6 +20,6 @@ folder = mediapool.GetCurrentFolder()
 
 for clip in folder.GetClipList():
     startTc = clip.GetClipProperty("Start TC")
-    changedTc = Timecode(fps, Timecode(fps, startTc)) + diff
+    changedTc = Timecode(videoFps, Timecode(videoFps, startTc)) + diff
     print(clip.GetName(), startTc, changedTc)
     clip.SetClipProperty("Start TC", str(changedTc))
