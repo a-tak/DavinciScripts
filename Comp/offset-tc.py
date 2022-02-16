e4af1ae1eb35a5665c80c9ccde1398b2ddcbee69
A-tak <hoge+git@a-tak.com>
2022-02-16 21:51:23 +0900

    タイムコード差分がマイナスになる場合に問題が起きるのを修正

--- Comp/offset-tc.py
+++ Comp/offset-tc.py
@@ -4,13 +4,14 @@ import DaVinciResolveScript as dvr_script
 from timecode import Timecode
 
 # ここにタイムコードとフレームレートを手打ち(笑)
-videoTc = "12:44:39:36"
-audioTc = "12:46:36:38"
-videoFps=59.94
-audioFps=60
+videoTimeCode = "12:40:15:49"
+audioTimeCode = "12:44:39:36"
+fps=59.94
 
-diff = Timecode(videoFps, audioTc) - Timecode(audioFps, videoTc)
-print("DiffTC",diff)
+videoTc = Timecode(fps, videoTimeCode)
+audioTc = Timecode(fps, audioTimeCode)
+
+diff =  audioTc - videoTc
 
 resolve = dvr_script.scriptapp("Resolve")
 projectManager = resolve.GetProjectManager()
@@ -20,6 +21,9 @@ folder = mediapool.GetCurrentFolder()
 
 for clip in folder.GetClipList():
     startTc = clip.GetClipProperty("Start TC")
-    changedTc = Timecode(videoFps, Timecode(videoFps, startTc)) + diff
-    print(clip.GetName(), startTc, changedTc)
+    if videoTc.frames > audioTc.frames:
+        changedTc = Timecode(fps, Timecode(fps, startTc)) - diff
+    else:
+        changedTc = Timecode(fps, Timecode(fps, startTc)) + diff
+    print(clip.GetName(), startTc, str(changedTc))
     clip.SetClipProperty("Start TC", str(changedTc))
