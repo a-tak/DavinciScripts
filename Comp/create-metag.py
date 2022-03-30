53d0822b1f46308bfea216d0b19dc6fb53413555
A-tak <hoge+git@a-tak.com>
2022-03-30 23:28:14 +0900

    画像出力まで

--- Comp/create-metag.py
+++ Comp/create-metag.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import DaVinciResolveScript as dvr_script
+import pathlib
+from PIL import Image, ImageDraw
 
 resolve = dvr_script.scriptapp("Resolve")
 projectManager = resolve.GetProjectManager()
@@ -8,13 +10,30 @@ mediapool = project.GetMediaPool()
 folder = mediapool.GetCurrentFolder()
 
 for clip in folder.GetClipList():
-    print(clip.GetMetadata("Camera Type"))
-    print(clip.GetMetadata("Lens Type"))
-    print(clip.GetMetadata("Camera Aperture"))
-    print(clip.GetMetadata("ISO"))
-    print(clip.GetMetadata("Shutter"))
-    print(clip.GetMetadata("Focal Point (mm)"))
-    print(clip.GetMetadata("Distance"))
-    print(clip.GetMetadata("White Balance Tint"))
-    print(clip.GetMetadata("White Point (Kelvin)"))
+    # パス取得
+    filePath = clip.GetClipProperty("File Path")
+    # filebase = pathlib.PurePath.name(filePath)
+    # folder = pathlib.Path(filePath).parent
+
+    # メタ情報取得
+    camera = clip.GetMetadata("Camera Type")
+    lens = clip.GetMetadata("Lens Type")
+    f = clip.GetMetadata("Camera Aperture")
+    iso = clip.GetMetadata("ISO")
+    shutter = clip.GetMetadata("Shutter")
+    focalPoint = clip.GetMetadata("Focal Point (mm)")
+    distance = clip.GetMetadata("Distance")
+    tint = clip.GetMetadata("White Balance Tint")
+    wb = clip.GetMetadata("White Point (Kelvin)")
+
+    # 画像生成(日本語非対応)
+    im = Image.new("RGB",(300,300), "blue")
+    draw = ImageDraw.Draw(im)
+    draw.text((0,0), "Camera : {}".format(camera))
+    # 動画ファイルの拡張子だけ変更した名前で画像を置く
+    savePath = pathlib.PurePath(filePath).with_suffix(".png")
+    print(savePath)
+    im.save(str(savePath))
+
+    # メディアプール登録
 
